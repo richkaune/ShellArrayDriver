28d8fbaa3d29ce056295e779a528ddddb50fab23
rkaune <rkaune@7HPL6Y1.Student.UGDSB.ED>
2019-06-05 11:23:58 -0400

    Pre-submission commit of ShellArrayDriver for COMP_2231

--- src/shellarraydriver/ShellArray.java
+++ src/shellarraydriver/ShellArray.java
@@ -1,6 +1,9 @@
 /**
+ * ShellArrayDriver.java
  *
- * @author rkaune
+ * Richard Kaune T00641439
+ * COMP 2231_SW2 Assignment 1 Question 1
+ * This application prompts the user to enter a number between 10 and 1000 and then performs a Shell sort on the array created
  */
 
 package shellarraydriver;
@@ -17,33 +20,43 @@ public class ShellArray
 {
     private Integer[] arr;
     
-    
+    /**
+     * ShellArray constructor, instantiates a ShellArray object with the size supplied by the user
+     * @param size 
+     */
     public ShellArray (int size)
     {
         //creates array 
         arr = new Integer[size];
-
+        
         Random generator = new Random();
 
-        //loads array with random ints
+        //loads array with random integers
         for(int x = 0; x < arr.length; x++)
         {
-                arr[x] = generator.nextInt(10);//set back to 1 million
+                arr[x] = generator.nextInt(10);
 
         }
     }
-    
+    /**
+     * Getter to access the array contents
+     * @return the array
+     */
     public Integer[] getArr ()
     {
         return this.arr;
     }
     
+    /**
+     * The sort method is implemented as a method of the ShellArray class
+     */
     void sort ()
     {
         int gap=arr.length;
         boolean swapflag = false;
         gap /=2;
-        //System.out.println(gap);
+        
+        // Shell algorithm based on provided pseudo-code
         while (gap>0)
         {
             swapflag = true;
@@ -62,12 +75,14 @@ public class ShellArray
                         swapflag = true;
                     }
                 }
-
             }
             gap/=2;
         }
     }
-        
+    /**
+     * The overridden toString() method to display the contents in a proper form
+     * @return 
+     */    
     @Override
     public String toString() 
     { 
--- src/shellarraydriver/ShellArrayDriver.java
+++ src/shellarraydriver/ShellArrayDriver.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ShellArrayDriver.java
+ *
+ * Richard Kaune T00641439
+ * COMP 2231_SW2 Assignment 1 Question 1
+ * This application prompts the user to enter a number between 10 and 1000 and then performs a Shell sort on the array created
  */
 
 package shellarraydriver;
@@ -9,10 +11,11 @@ package shellarraydriver;
 import java.util.Scanner;
 
 /**
- *
+ * Driver class. 
  * @author rkaune
  */
-public class ShellArrayDriver {
+public class ShellArrayDriver 
+{
     ShellArray ob;
   
     // Driver method 
@@ -20,18 +23,23 @@ public class ShellArrayDriver {
     { 
         int userInput=0;
         Scanner input = new Scanner(System.in);
+        
+        // prompt the user for a number of integers to sort between 10 and 1000
         do
         {
-            System.out.println("Please enter a number between 10 and 1000 to sort with the shell sort ");
+            System.out.print("Please enter a number between 10 and 1000 numbers to sort with the shell sort: ");
             userInput = input.nextInt();
         } while (userInput < 10 || userInput > 1000);
         
+        // create the ShellArray object from the user input and display the starting condition
         ShellArray ob = new ShellArray(userInput);
         System.out.println("Array before sorting"); 
         System.out.println(ob.toString());
 
+        // sort the created array
         ob.sort();
-  
+
+        // display the arrays finishing condition
         System.out.println("Array after sorting"); 
         System.out.println(ob.toString());
         
